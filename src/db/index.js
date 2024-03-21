b91fb04d2cef4e7438eb2d6b21f0e9cd2a10aff9
AI Arif <contact.arif20@gmail.com>
2024-03-21 15:51:17 +0600

    Project Setup Complete

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGO_URI}/${DB_NAME}`
+    );
+    console.log(
+      `MongoDB connection SUCCESS!", ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MongoDB connection FAIL!", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
