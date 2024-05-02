d20c7c9d3d8a7eb3c6f6d7955061787ad2379696
nuralam <nurcse253@gmail.com>
2024-05-03 00:50:52 +0600

    test api

--- src/controllers/Admin/clinicaltest.controller.js
+++ src/controllers/Admin/clinicaltest.controller.js
@@ -1,36 +1,44 @@
 import ClinicalTest from "../../models/clinicaltest.model.js";
 import sendResponse from "../../utils/sendResponse.js";
-
+import {faker} from '@faker-js/faker'
 
 export const addTest = async (req, res) => {
   try {
     const { testName, testDetails } = req.body;
-    const newClinicalTest = new ClinicalTest({testName,testDetails});
+    const newClinicalTest = new ClinicalTest({ testName, testDetails });
     await newClinicalTest.save();
-   sendResponse(res, 200, true, "Successfully created clinical test");
+    // let dummyData = []
+    // for(let i=0;i<50;i++){
+    //    dummyData.push({
+    //        testName: faker.person.fullName(),
+    //        testDetails: faker.lorem.lines({ min: 1, max: 5})
+    //    })
+    // }
+   // await ClinicalTest.insertMany(dummyData)
+    sendResponse(res, 200, true, "Successfully created clinical test");
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return sendResponse(res, 500, false, error.message);
   }
 };
 
-
-
 export const getTest = async (req, res) => {
   try {
-    const tests = await ClinicalTest.find()
-    sendResponse(res,200,true,"Showing results",tests)
+    const tests = await ClinicalTest.find();
+    sendResponse(res, 200, true, "Showing results", tests);
     
   } catch (error) {
     return sendResponse(res, 500, false, error.message);
   }
 };
 
-
 export const updateTest = async (req, res) => {
-    const { testName, testDetails } = req.body;
- 
+  const { testName, testDetails } = req.body;
+  const { id } = req.params;
+
   try {
+    await ClinicalTest.updateOne({ _id: id }, { testName, testDetails });
+    sendResponse(res, 200, true, "Successfully updated clinical test");
 
   } catch (error) {
     return sendResponse(res, 500, false, error.message);
@@ -38,6 +46,12 @@ export const updateTest = async (req, res) => {
 };
 export const deleteTest = async (req, res) => {
   try {
+    const { id } = req.params;
+    await ClinicalTest.deleteOne({_id:id})
+    sendResponse(res, 200, true, "Successfully deleted clinical test");
+
+
+
   } catch (error) {
     return sendResponse(res, 500, false, error.message);
   }
