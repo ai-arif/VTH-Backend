65ea85d2a31521ac33587955b842215ae759ee48
nuralam <nurcse253@gmail.com>
2024-05-14 00:26:54 +0600

    dynamic test post api

--- src/controllers/Admin/clinicaltest.controller.js
+++ src/controllers/Admin/clinicaltest.controller.js
@@ -9,8 +9,8 @@ export const addTest = async (req, res) => {
   try {
     const { testName, testDetails } = req.body;
     const newClinicalTest = new ClinicalTest({ testName, testDetails });
-    const newTest=await newClinicalTest.save();
-    
+    const newTest = await newClinicalTest.save();
+
     sendResponse(res, 200, true, "Successfully created clinical test", newTest);
   } catch (error) {
     console.log(error);
@@ -20,7 +20,7 @@ export const addTest = async (req, res) => {
 
 export const getTest = async (req, res) => {
   const page = parseInt(req.query.currentPage) || 1;
-  const limit = parseInt(req.query.limit) || ''
+  const limit = parseInt(req.query.limit) || "";
   const sort = -1;
   try {
     const totalTest = await ClinicalTest.countDocuments();
@@ -70,131 +70,129 @@ export const deleteTest = async (req, res) => {
   }
 };
 
-
 // TEST PARAMETER
 
-export const AddParameter = async(req,res)=> {
-  try{
-    const {name,testId} = req.body
-    const newParameter = new TestParameter({name,test:testId})
-    await newParameter.save()
+export const AddParameter = async (req, res) => {
+  try {
+    const { name, testId } = req.body;
+    const newParameter = new TestParameter({ name, test: testId });
+    await newParameter.save();
     sendResponse(res, 200, true, "Successfully added parameter");
-  }catch(error){
-    sendResponse(res,500,false,error.message)
+  } catch (error) {
+    sendResponse(res, 500, false, error.message);
   }
-}
+};
 
-
-
-export const getParameter = async(req,res)=> {
-  try{
-    const {id} = req.params
-    const parameters = await TestParameter.find({test:id})
-    sendResponse(res, 200, true, "Successfully fetched parameter",{data:parameters});
-  }catch(error){
-    sendResponse(res,500,false,error.message)
+export const getParameter = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const parameters = await TestParameter.find({ test: id });
+    sendResponse(res, 200, true, "Successfully fetched parameter", {
+      data: parameters,
+    });
+  } catch (error) {
+    sendResponse(res, 500, false, error.message);
   }
-}
+};
 
-
-export const deleteParameter = async(req,res)=> {
-  try{
-    const {id} = req.params
-    await TestParameter.deleteOne({_id:id})
+export const deleteParameter = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await TestParameter.deleteOne({ _id: id });
     sendResponse(res, 200, true, "Successfully deleted parameter");
-  }catch(error){
-    sendResponse(res,500,false,error.message)
+  } catch (error) {
+    sendResponse(res, 500, false, error.message);
   }
-}
+};
 
-
-export const updateParameter = async(req,res)=> {
-  try{
-    const {id} = req.params
-    await TestParameter.updateOne({_id:id},{...req.body})
+export const updateParameter = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await TestParameter.updateOne({ _id: id }, { ...req.body });
     sendResponse(res, 200, true, "Successfully updated parameter");
-  }catch(error){
-    sendResponse(res,500,false,error.message)
+  } catch (error) {
+    sendResponse(res, 500, false, error.message);
   }
-}
-
-
-
+};
 
 //SUB PARAMETER
-export const AddSubParameter = async(req,res)=> {
-  try{
-    const {sub_parameter_type,text,check,test_parameter} = req.body
-     console.log(req.body)
+export const AddSubParameter = async (req, res) => {
+  try {
+    const { sub_parameter_type, text, check, test_parameter } = req.body;
+    console.log(req.body);
     const newSubParameter = new TestSubParameter({
       sub_parameter_type,
       text,
       check,
-      test_parameter
-    })
-    await newSubParameter.save()
+      test_parameter,
+    });
+    await newSubParameter.save();
     sendResponse(res, 200, true, "Successfully added sub parameter");
-  }catch(error){
-    sendResponse(res,500,false,error.message)
+  } catch (error) {
+    sendResponse(res, 500, false, error.message);
   }
-}
+};
 
-
-export const getAllSubParameter = async(req,res)=> {
-  try{
-    const sub_parameters = await TestSubParameter.find()
-    sendResponse(res, 200, true, "Successfully fetched sub parameter",{data:sub_parameters});
-  
-  }catch(error){
-    sendResponse(res,500,false,error.message)
+export const getAllSubParameter = async (req, res) => {
+  try {
+    const sub_parameters = await TestSubParameter.find();
+    sendResponse(res, 200, true, "Successfully fetched sub parameter", {
+      data: sub_parameters,
+    });
+  } catch (error) {
+    sendResponse(res, 500, false, error.message);
   }
-}
+};
 
-
-
-export const getSubParameter = async(req,res)=> {
-  try{
-    const {id} = req.params
-    const sub_parameter = await TestSubParameter.find({test_parameter:id})
-    sendResponse(res, 200, true, "Successfully fetched sub parameter",{data:sub_parameter});
-  }catch(error){
-    sendResponse(res,500,false,error.message)
+export const getSubParameter = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const sub_parameter = await TestSubParameter.find({ test_parameter: id });
+    sendResponse(res, 200, true, "Successfully fetched sub parameter", {
+      data: sub_parameter,
+    });
+  } catch (error) {
+    sendResponse(res, 500, false, error.message);
   }
-}
+};
 
-
-
-export const deleteSubParameter = async(req,res)=> {
-  try{
-    const {id} = req.params
-    await TestSubParameter.deleteOne({_id:id})
+export const deleteSubParameter = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await TestSubParameter.deleteOne({ _id: id });
     sendResponse(res, 200, true, "Successfully deleted sub parameter");
-  }catch(error){
-    sendResponse(res,500,false,error.message)
+  } catch (error) {
+    sendResponse(res, 500, false, error.message);
   }
-}
+};
 
-
-export const updateSubParameter = async(req,res)=> {
-  try{
-    const {id} = req.params
-    await TestSubParameter.updateOne({_id:id},{...req.body})
+export const updateSubParameter = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await TestSubParameter.updateOne({ _id: id }, { ...req.body });
     sendResponse(res, 200, true, "Successfully updated sub parameter");
-  }catch(error){
-    sendResponse(res,500,false,error.message)
+  } catch (error) {
+    sendResponse(res, 500, false, error.message);
   }
-}
-
-
+};
 
 //APPOINTMENT TEST
-export const AddAppointmentTest = async(req,res)=> {
-  try{
-     const newAppointmentTest = new AppointmentTest({...req.body})
-     await newAppointmentTest.save()
+export const AddAppointmentTest = async (req, res) => {
+  try {
+    const { caseNo, test } = req.body;
+    
+    const existTest = await AppointmentTest.findOne({ caseNo });
 
+    if (existTest) {
+     let result = [...existTest.test]
+     result.push(test)
+     await AppointmentTest.updateOne({ caseNo }, { test: result });
+    } else {
+      const newAppointmentTest = new AppointmentTest({ caseNo,test});
+      await newAppointmentTest.save();
+    }
     sendResponse(res, 200, true, "Successfully created appointment test");
-  }catch(error){
-    sendResponse(res,500,false,error.message)
+  } catch (error) {
+    sendResponse(res, 500, false, error.message);
   }
-}
+};
