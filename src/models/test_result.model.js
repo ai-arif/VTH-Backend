7a3b7a66fe4d3a6cc7113c8ba3309f5d71708cf6
tapfer-abdullah <tapfer.abdullah@gmail.com>
2024-05-23 14:45:59 +0600

    modified test result model and controller

--- src/models/test_result.model.js
+++ src/models/test_result.model.js
@@ -7,6 +7,7 @@ const dynamicSubSchema = new Schema({}, { strict: false });
 const testResultSchema = new Schema({
     testId: { type: String, required: true },
     appointmentId: { type: String, required: true },
+    name: { type: String, required: true },
     phone: { type: String, required: true },
     data: { type: dynamicSubSchema }
 }, { timestamps: true });
@@ -14,3 +15,40 @@ const testResultSchema = new Schema({
 // Create the model
 const TestResult = mongoose.model('TestResult', testResultSchema);
 export default TestResult;
+
+
+// {
+//     "Gestation sac param#Absent": false,
+//         "Gestation sac param#Present": true,
+//             "Gestation sac param#Present&Rt horn": true,
+//                 "Gestation sac param#Present&Lt horn": true,
+//                     "Gestation sac param#Present&Number": "20",
+//                         "Gestation sac param#input sub": "param sub 2",
+//                             "Gestation sac param#none": true,
+//                                 "param-2#sub-text": "param sub 2 of 2",
+//                                     "param-2#Absence": true,
+//                                         "param-2#Present": false
+// }
+
+// const a = {
+//     "Gestation sac param": [
+//         { Absent: false },
+//         {
+//             Present: true, "Present&": [
+//                 { "Rt horn": true },
+//                 { "Lt horn": true },
+//                 { Number: 20 },
+//             ]
+//         },
+
+//         { "input sub": "param sub 2}" },
+//         { none: true },
+
+//     ],
+
+//     "param-2": [
+//         { Absence: true },
+//         { Present: false },
+//     ]
+
+// }
