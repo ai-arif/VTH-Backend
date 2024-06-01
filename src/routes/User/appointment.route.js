de4aa4b1093f696cbb478db4e9f5277eab775383
tapfer-abdullah <tapfer.abdullah@gmail.com>
2024-06-01 17:34:40 +0600

    upload image using google cloud done

--- src/routes/User/appointment.route.js
+++ src/routes/User/appointment.route.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { createAppointment, getAllAppointments } from "../../controllers/User/appointment.controller.js";
 import { verifyJWT } from "../../middlewares/auth.middleware.js";
-import configureMulter from "../../utils/multerConfig.js";
+import { configureMulter } from "../../utils/multerConfig.js";
 const appointmentRouter = Router();
-
 const upload = configureMulter("uploads");
-appointmentRouter.post("/", verifyJWT, upload.single('image'), createAppointment);
+
+appointmentRouter.post("/", verifyJWT, upload.array('images', 5), createAppointment);
 appointmentRouter.get("/", verifyJWT, getAllAppointments);
 
 export default appointmentRouter;
